4eae2dd3e27c97882661fee33d6f0040eaf56a8c
Ana Castro <arvc@ua.pt>
2023-04-14 14:26:21 +0100

    +update controller ImagesController to return db info to view gallery.index +update view gallery.index to show data from controller

--- grid-layout/app/Http/Controllers/ImagesController.php
+++ grid-layout/app/Http/Controllers/ImagesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 
 use App\Models\Photo;
+use Illuminate\Support\Facades\DB;
 
 class ImagesController extends Controller
 {
@@ -12,7 +13,40 @@ class ImagesController extends Controller
     {
         $photosDB = Photo::all();
 
-        return view('gallery.index', compact('photosDB'));
+        $gallery = array();
+        foreach ($photosDB as $photo) {
+            foreach ($photo->photo_details as $photo_detail){
+                $sizeTeste = DB::table('sizes')->where('photo_id','like',$photo_detail['id'])->first();
+                $size_details = json_decode($sizeTeste->size_details);
+                foreach ($size_details as $detail){
+                    if ($detail->label === "Medium 640"){
+                        $size_details = $detail;
+                    }
+                }
+                $gallery[] = [
+                    "photo_id"=>$photo_detail['id'],
+                    "page"=>$photo['page'],
+                    "pages"=>$photo['pages'],
+                    "perpage"=>$photo['perpage'],
+                    "total"=>$photo['total'],
+                    "photo_owner"=>$photo_detail['owner'],
+                    "photo_secret"=>$photo_detail['secret'],
+                    "photo_server"=>$photo_detail['server'],
+                    "photo_farm"=>$photo_detail['farm'],
+                    "photo_title"=>$photo_detail['title'],
+                    "photo_ispublic"=>$photo_detail['ispublic'],
+                    "photo_isfriend"=>$photo_detail['isfriend'],
+                    "photo_isfamily"=>$photo_detail['isfamily'],
+                    "size_canblog"=> $sizeTeste->canblog,
+                    "size_canprint"=> $sizeTeste->canprint,
+                    "size_candownload"=> $sizeTeste->candownload,
+                    "size_details"=> $size_details,
+                ];
+            }
+
+        }
+
+        return view('gallery.index', compact('gallery'));
     }
 
 }
--- grid-layout/resources/views/gallery/index.blade.php
+++ grid-layout/resources/views/gallery/index.blade.php
@@ -3,6 +3,21 @@
     <h1>Galeria de imagens</h1>
 
     <div class="row">
-
+        <div class="container">
+            @foreach ($gallery as $photo)
+                <div class="test_box box-01 grid-item" style="width: 100%">
+                    <div class="inner" style="background-image: url('{{$photo['size_details']->source}}')">
+                        <a href="{{$photo['size_details']->url}}" class="test_click" target="_blank">
+                            <div class="flex_this">
+                                <h3 class="test_title">
+                                    {{$photo['photo_title']}}
+                                </h3>
+                                <span class="test_link">Link</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
